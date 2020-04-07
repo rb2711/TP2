74f312d00cebd1aea4b22a52c25fbd3f0b456078
rb2711 <rodobertero@gmail.com>
2020-04-07 20:57:10 -0300

    Initial commit

---
+++ app/src/main/java/com/example/tp2/MainActivity.java
@@ -0,0 +1,110 @@
+package com.example.tp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+private EditText etDolar, etEuro;
+private RadioButton rbEuroDolar, rbDolarEuro;
+private TextView tvResultado, tvError;
+    private MainViewModel mv;
+    private Button btConvertir;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        iniciarVista();
+
+        mv= ViewModelProviders.of(this).get(MainViewModel.class);
+        mv.getEuroDolar().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean aBoolean) {
+                rbEuroDolar.setChecked(aBoolean);
+            }
+        });
+        mv.getDolarEuro().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean aBoolean) {
+                rbDolarEuro.setChecked(aBoolean);
+            }
+        });
+        mv.getMostrarRes().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                tvResultado.setText(s);
+            }
+        });
+        mv.getError().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean aBoolean) {
+                tvError.setEnabled(aBoolean);
+            }
+            
+        });
+
+    }
+
+    private void iniciarVista(){
+        etDolar=findViewById(R.id.etDolar);
+        etEuro=findViewById(R.id.etEuro);
+        rbDolarEuro=findViewById(R.id.rbDolarEuro);
+        rbEuroDolar=findViewById(R.id.rbEuroDolar);
+        tvResultado=findViewById(R.id.tvMostrarRes);
+        btConvertir = findViewById(R.id.btConvertir);
+        tvError=findViewById(R.id.tvError);
+
+        rbEuroDolar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mv.cambiarEstadoDolarEuro();
+                etDolar.setEnabled(false);
+                etDolar.setText("");
+                etEuro.setEnabled(true);
+            }
+        });
+
+        rbDolarEuro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mv.cambiarEstadoEuroDolar();
+                etEuro.setEnabled(false);
+                etEuro.setText("");
+                etDolar.setEnabled(true);
+            }
+        });
+
+       /* btConvertir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                    mv.hacerCambio(etEuro.getText().toString(),etDolar.getText().toString());
+                }
+        });*/
+
+        btConvertir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (rbEuroDolar.isChecked()) {
+                    mv.hacerCambio(etEuro.getText().toString(), "ED");
+                }
+                else{
+                    mv.hacerCambio(etDolar.getText().toString(),"DE");
+                }
+            }
+
+        });
+    }
+
+
+}
---
+++ app/src/main/java/com/example/tp2/MainViewModel.java
@@ -0,0 +1,98 @@
+package com.example.tp2;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+
+    private MutableLiveData<String> mostrarRes;//LiveData son tipos de variables que la vistaobserva con observe
+    private MutableLiveData<Boolean> euroDolar;
+    private MutableLiveData<Boolean> dolarEuro;
+    private MutableLiveData<Boolean> error;
+
+    public LiveData<String> getMostrarRes(){
+        if (mostrarRes==null){
+            mostrarRes = new MutableLiveData<String>();
+        }
+        return mostrarRes;
+    }
+
+    public LiveData<Boolean> getEuroDolar(){
+        if (euroDolar==null){
+            euroDolar = new MutableLiveData<Boolean>();
+        }
+        return euroDolar;
+    }
+
+    public LiveData<Boolean> getDolarEuro(){
+        if (dolarEuro==null){
+            dolarEuro = new MutableLiveData<Boolean>();
+        }
+        return dolarEuro;
+    }
+    public LiveData<Boolean> getError(){
+        if (error == null) {
+            error=new MutableLiveData<Boolean>();
+        }
+        return error;
+    }
+
+
+    public void cambiarEstadoEuroDolar(){
+
+        euroDolar.setValue(false);
+    }
+    public void cambiarEstadoDolarEuro(){
+
+        dolarEuro.setValue(false);
+    }
+
+   /* public void hacerCambio(String vEuro,String vDolar){
+        double val;
+        double res=0;
+        try {
+            if(vEuro!=null) {
+                val = Double.parseDouble(vEuro);
+                res = val * 1.09;
+                error.setValue(false);
+                mostrarRes.setValue(res + "");
+            }
+            else if (vDolar!=null){
+                val = Double.parseDouble(vDolar);
+                res = val * 0.92;
+                error.setValue(false);
+                mostrarRes.setValue(res + "");
+            }
+            else {error.setValue(true);
+
+            }
+         }
+        catch (Exception e){
+            error.setValue(true);
+        }
+    }*/
+
+    public void hacerCambio(String valor,String cambio){
+        double val;
+        double res=0;
+        try {
+            val = Double.parseDouble(valor);
+
+            switch (cambio) {
+                case "ED": {
+                    res = val * 1.09;
+                    break;
+                }
+                case "DE": {
+                    res = val * 0.92;
+                    break;
+                }
+            }
+            mostrarRes.setValue(res + "");
+            error.setValue(false);
+        }catch (Exception e){
+            error.setValue(true);
+        }
+    }
+}
